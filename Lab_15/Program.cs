fced9b03f4f51e5226afb5a32078db9af51f7627
Дмитрий Минаков <m1nak0vd@yandex.ru>
2022-01-23 11:54:49 +0300

    Добавьте файлы проекта.

---
+++ Lab_15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IMovable
+    {
+        void Move();
+        void Brake();
+    
+    }
+    class Mercedes : IMovable
+    {
+        public void Brake()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
